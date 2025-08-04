ec4cfa57508fb061ef14bd9fd6a6c14b681a6416
bored-arvi <aravinth3007k@gmail.com>
2025-08-05 03:58:03 +0530

    opt 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,9 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center px-8 py-4 bg-transparent shadow-none animate-fadeDown">
       <div className="font-heading text-2xl font-light text-white">
-        Java Report Generator
+        Javord
       </div>
+      
     </header>
   );
 }
