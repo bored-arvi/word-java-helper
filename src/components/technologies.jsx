232f6a53590ef3953cdf84f2903b456dd2da180d
forlorn-arvi <24f2008556@ds.study.iitm.ac.in>
2025-08-05 04:50:39 +0530

    added background

--- src/components/technologies.jsx
+++ src/components/technologies.jsx
@@ -1,17 +1,17 @@
 import { FaPython, FaReact, FaNodeJs, FaGithub } from "react-icons/fa";
 import { SiTailwindcss, SiOpenjdk, SiVite } from "react-icons/si";
 
-
 const techIcons = [FaReact, SiTailwindcss, SiOpenjdk, SiVite, FaNodeJs, FaGithub, FaPython];
 
 export default function Technologies() {
   return (
-    <section className="w-full overflow-hidden py-8 relative">
-      {/* Conveyor Belt */}
-      <div className="flex animate-tech-scroll">
-        {[...techIcons, ...techIcons].map((Icon, i) => (
-          <Icon key={i} size={80} className="mx-12 text-white flex-shrink-0" />
-        ))}
+    <section className="w-full bg-black py-5 px-0">
+      <div className="overflow-hidden">
+        <div className="flex animate-tech-scroll py-6">
+          {[...techIcons, ...techIcons].map((Icon, i) => (
+            <Icon key={i} size={60} className="mx-12 text-white flex-shrink-0" />
+          ))}
+        </div>
       </div>
     </section>
   );
