7a6bdcbe166a443b8850647217e4f174cae73b02
bored-arvi <aravinth3007k@gmail.com>
2025-08-05 04:39:07 +0530

    created new layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import React from "react";
-import Background from "./components/background";
 import Header from "./components/Header";
 import Hero from "./components/MainSection";
+import Technologies from "./components/technologies";
+import Features from "./components/features";
+import About from "./components/About";
+import Background from "./components/background"; // your Vanta background
 
 export default function App() {
   return (
-    <div className="relative min-h-screen text-white">
+    <div className="relative min-h-screen text-white font-body">
+      {/* Animated Background */}
       <Background />
+
+      {/* Fixed Transparent Header */}
       <Header />
+
+      {/* Hero Section (left aligned) */}
       <Hero />
+
+      {/* Technologies Carousel */}
+      <Technologies />
+
+      {/* Glass Features Cards */}
+      <Features />
+
+      {/* About Me Section */}
+      <About />
     </div>
   );
 }
