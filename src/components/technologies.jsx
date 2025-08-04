8e33a641cf9b5f210b447c59206c257cbeec4208
bored-arvi <aravinth3007k@gmail.com>
2025-08-05 04:39:57 +0530

    scroll bar done

---
+++ src/components/technologies.jsx
@@ -0,0 +1,18 @@
+import { FaReact, FaNodeJs, FaGithub } from "react-icons/fa";
+import { SiTailwindcss, SiOpenjdk, SiVite } from "react-icons/si";
+
+
+const techIcons = [FaReact, SiTailwindcss, SiOpenjdk, SiVite, FaNodeJs, FaGithub];
+
+export default function Technologies() {
+  return (
+    <section className="w-full overflow-hidden py-8 relative">
+      {/* Conveyor Belt */}
+      <div className="flex animate-tech-scroll">
+        {[...techIcons, ...techIcons].map((Icon, i) => (
+          <Icon key={i} size={80} className="mx-12 text-white flex-shrink-0" />
+        ))}
+      </div>
+    </section>
+  );
+}
