cac9a4c365de0ea63035bd0ec84eb0bbadd28b49
forlorn-arvi <24f2008556@ds.study.iitm.ac.in>
2025-08-05 03:58:03 +0530

    opt 1

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -5,6 +5,9 @@ export default function Hero() {
     <section className="relative flex justify-center items-center h-screen px-4">
       {/* Glass Card */}
       <div className="bg-white/10 backdrop-blur-md border border-white/20 rounded-2xl shadow-2xl p-10 md:p-16 max-w-6xl text-center animate-fadeInUp">
+      <div className="absolute top-0 left-0 w-16 h-1 bg-white/30"></div>
+    <div className="absolute top-0 left-0 w-1 h-16 bg-white/30"></div>
+
         <h1 className="font-heading font-bold text-5xl sm:text-6xl lg:text-8xl tracking-tight leading-tight text-white mb-8 drop-shadow-lg">
           Javord
         </h1>
@@ -12,11 +15,12 @@ export default function Hero() {
           Upload your question file and Java solutions to automatically generate
           a combined Word report with formatted code and execution screenshots.
         </p>
-        <button className="bg-white/10 backdrop-blur-md border border-white/30 text-white px-10 py-4 rounded-full text-xl font-semibold hover:bg-white hover:text-black transition transform hover:scale-110 hover:shadow-xl duration-300">
-  Upload Now
-</button>
+        <button className="font-heading bg-white/10 backdrop-blur-md border border-white/30 text-white px-10 py-4 rounded-full text-xl font-semibold hover:bg-white hover:text-black transition transform hover:scale-110 hover:shadow-xl duration-300">
+            Upload Now
+        </button>
 
       </div>
+      
     </section>
   );
 }
