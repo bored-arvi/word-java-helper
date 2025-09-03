d2d52866b6172fe15d3139bb06a231e0c5f20810
bored-arvi <aravinth3007k@gmail.com>
2025-09-04 02:20:47 +0530

    generated backend dependencies and structure

---
+++ backend/javarunner/src/main/java/com/javarunner/javarunner/JavarunnerApplication.java
@@ -0,0 +1,13 @@
+package com.javarunner.javarunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavarunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavarunnerApplication.class, args);
+	}
+
+}
---
+++ backend/javarunner/src/test/java/com/javarunner/javarunner/JavarunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javarunner.javarunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
