e15f0132a592188bfc8bd1568fa9444c6c81271b
bored-arvi <aravinth3007k@gmail.com>
2025-09-04 03:13:42 +0530

    created application

--- backend/javarunner/src/main/java/com/javarunner/javarunner/JavarunnerApplication.java
+++ backend/javarunner/src/main/java/com/javarunner/javarunner/JavarunnerApplication.java
@@ -1,4 +1,4 @@
-package com.javarunner.javarunner;
+package com.javarunner.fileprocessor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavarunnerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavarunnerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(JavarunnerApplication.class, args);
+    }
 }
